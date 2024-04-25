3a6a3b9098df9751e8cbbcdeaa809a00b78265ab
Christen <christen.foad@gmail.com>
2024-04-25 13:40:44 +0200

    Day2_Part2

--- HrBlazorWASM/Models/Employee.cs
+++ HrBlazorWASM/Models/Employee.cs
@@ -3,9 +3,9 @@
     public class Employee
     {
         public int Id { get; set; }
-
+        
         public string Name { get; set; }
-
+        
         public int Salary { get; set; }
 
         public string Image { get; set; }
