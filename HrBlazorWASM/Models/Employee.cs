849844985c4229cc2b6e6e78dcd3478ef550d41e
Christen <christen.foad@gmail.com>
2024-04-27 09:37:45 +0300

    Day3 _Update

--- HrBlazorWASM/Models/Employee.cs
+++ HrBlazorWASM/Models/Employee.cs
@@ -1,14 +1,22 @@
-﻿namespace HrBlazorWASM.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HrBlazorWASM.Models
 {
     public class Employee
     {
         public int Id { get; set; }
-        
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3,ErrorMessage ="Name Must Be More Than 2 Char")]
         public string Name { get; set; }
-        
+
+        [Range(6000,20000)]
         public int Salary { get; set; }
 
+        
         public string Image { get; set; }
+
         public int Dept_Id { get; set; }
 
         public override string ToString()
--- HrBlazorWASM/Program.cs
+++ HrBlazorWASM/Program.cs
@@ -18,7 +18,11 @@ namespace HrBlazorWASM
             builder.Services.AddScoped<EmployeeService>();
             builder.Services.AddScoped<DepartmentService>();
             //register
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
+            builder.Services.AddScoped
+                (sp => new HttpClient 
+                { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+                });
 
             await builder.Build().RunAsync();
         }
